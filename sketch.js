3729ec8aa39340f23494f1f7970eddff48adc5a2
Vitoriapinaaraujo <vitoriadepina@gmail.com>
2024-10-03 12:07:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+// divertidamente, LIVRE-COMEDIA 
+// guardioes da galaxia,12-fantasia e AÇAO
+// shrek,LIVRE-COMEDIA
+// o rei leao,LIVRE-COMEDIA
+// o diario da tati,LIVRE-COMEDIA
+
+
+
+
+
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+ let recomendaçao=geraRecomendaçao();
+ text(recomendaçao, width / 2, height / 2);  
+}
+  
+function geraRecomendaçao() {
+  
+
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
